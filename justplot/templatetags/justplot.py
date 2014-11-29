f07be895de421e37ed0ef44fe653854cb5c24afb
chillaranand <anand21nanda@gmail.com>
2014-11-29 21:35:30 +0530

    added doc strings to functions

--- justplot/templatetags/justplot.py
+++ justplot/templatetags/justplot.py
@@ -5,47 +5,54 @@ from django import template
 
 from ..template import CHART_HTML
 
-
 register = template.Library()
 
+
 def chart(name, parser, token):
+    """
+    Template tag parser function which takes parameters and
+    returns a Node object.
+    :param name: Name of template tag.
+    :param parser: Template parser object.
+    :param token: Token currently being processed by parser.
+    :return: Template Node object.
+    """
     args = token.split_contents()
-    args = parse_options(args)
     return ChartNode(name=name, args=args)
 
 
-def parse_options(source):
-    """parses chart tag options"""
-    options = {}
-    tokens = [t.strip() for t in source.split('=')]
-
-    name = tokens[0]
-    for token in tokens[1:-1]:
-        value, next_name = token.rsplit(' ', 1)
-        options[name.strip()] = value
-        name = next_name
-    options[name.strip()] = tokens[-1].strip()
-    return options
-
-
 class ChartNode(template.Node):
-
+    """
+    Renderer for node object.
+    """
     def __init__(self, name, args):
+        """
+        :param name: Name of template tag.
+        :param args: Arguments given for template tag.
+        :return: None
+        """
         self.name = name
         self.args = args
-        self.options = dict(i.split('=') for i in args[2:])
+        self.options = dict(i.split('=') for i in self.args[2:])
         for k, v in self.options.items():
             self.options[k] = template.Variable(v)
 
     def render(self, context):
         for k, v in self.options.items():
             self.options[k] = self.options[k].resolve(context)
-        if isinstance(self.options['data'], basestring):
-            self.options['data'] = json.loads(self.options['data'])
-        return CHART_HTML.format(id=self.options['id'],
-                                 data=[self.options['data']],
-                                 height='100px', width='200px')
-        #return self.args, self.options
+        # if isinstance(self.options['data'], str):
+        #     self.options['data'] = json.loads(self.options['data'])
+        self.options = add_defaults(self.options)
+        return CHART_HTML.format(**self.options)
+
+
+def add_defaults(d):
+    """
+    Add defualt options to user options.
+    """
+    d['height'] = d.get('height', '350px')
+    d['width'] = d.get('width', '700px')
+    return d
+
 
 register.tag('line_chart', functools.partial(chart, 'LineChart'))
-register.tag('pie_chart', functools.partial(chart, 'PieChart'))
